87b7c2d01cabbf3011b57b00eb530bbde95f16df
Denis Beskorovainyi <74551976+DBeskorovainyiEPAM@users.noreply.github.com>
2022-12-01 14:07:10 +0400

    Added config for image-webpack-loader

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,31 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('file-loader')
+      .test(/\.(gif|png|jpe?g|svg)$/i)
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        mozjpeg: {
+          progressive: true,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.90],
+          speed: 4
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75
+        }
+      });
+  }
 });
