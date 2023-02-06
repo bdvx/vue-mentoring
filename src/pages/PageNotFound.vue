6f8c192535ac208872a49294237074325ac6a256
Denis Beskorovainyi <74551976+DBeskorovainyiEPAM@users.noreply.github.com>
2023-02-07 02:36:20 +0400

    Added Not Found page

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class="not-found">
+        <span class="not-found-text">
+        Page not found
+        </span>
+    </div>
+</template>
+<script>
+export default {
+  name: 'PageNotFound',
+};
+</script>
+<style scoped>
+.not-found{
+    height: 775px;
+    background-color: #232323;
+    padding: 10px;
+}
+.not-found-text{
+    color: white;
+    font-weight: 300;
+    font-size: 56px;
+    margin-top: 200px;
+    display: block;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Main from '@/pages/Main.vue';
 import MovieItem from '@/pages/MovieItem.vue';
+import PageNotFound from '@/pages/PageNotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,10 @@ const router = createRouter({
       path: '/movie/:id',
       name: 'movie',
       component: MovieItem,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: PageNotFound,
     },
   ],
 });
